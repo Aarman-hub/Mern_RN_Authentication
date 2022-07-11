b8bde44bf2e209fd9848f5d7d3efa46a17f44888
Aarman-hub <arman.dnt84@gmail.com>
2022-07-11 08:02:08 +0600

    Register

--- mern_rn_auth/App.js
+++ mern_rn_auth/App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './src/screens/auth/Login';
+import Register from './src/screens/auth/Register';
 
 export default function App() {
   return (
     <>
       <StatusBar style='auto' />
-      <Login />    
+      <Register />    
     </>
   );
 }
--- mern_rn_auth/src/screens/auth/Register.js
+++ mern_rn_auth/src/screens/auth/Register.js
@@ -1,11 +1,73 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { ButtonText, Colors, InnerContainer, Line, MsgBox, PageLogo, PageTitle, StyledButton, StyledContainer, StyledFormArea, Subtitle } from '../../components/Styles'
+import { Formik } from 'formik'
+import CustomInput from '../../components/CustomInput'
+import {Fontisto} from '@expo/vector-icons';
+
+
+const { dirkLight, primary } = Colors;
 
 const Register = () => {
+  const [hidePassword, setHidePassword] = useState(true)
   return (
-    <View>
-      <Text>Register</Text>
-    </View>
+    <StyledContainer>
+        <InnerContainer>
+            <PageLogo source={require('../../../assets/logo.jpg')} />
+            <PageTitle>ArNa</PageTitle>
+            <Subtitle>Register</Subtitle>
+            <Formik 
+                initialValues={{email:"", username:"", password:""}}
+                onSubmit={(values)=>{
+                    console.log(values);
+                }}
+            >
+                {({handleChange, handleBlur, handleSubmit,values})=><StyledFormArea>
+                    <CustomInput
+                        label="Email Address"
+                        icon="mail"
+                        placeholder="mail@gmail.com"
+                        placeholderTextColor={dirkLight}
+                        onChangeText={handleChange('email')}
+                        onBlur={handleBlur('email')}
+                        values={values.email}
+                        keyboardType="email-address"
+                    />
+                    <CustomInput
+                        label="Username"
+                        icon="person"
+                        placeholder="Username"
+                        placeholderTextColor={dirkLight}
+                        onChangeText={handleChange('username')}
+                        onBlur={handleBlur('username')}
+                        values={values.username}
+                    />
+                    <CustomInput
+                        label="Password"
+                        icon="lock"
+                        placeholder="password"
+                        placeholderTextColor={dirkLight}
+                        onChangeText={handleChange('password')}
+                        onBlur={handleBlur('password')}
+                        values={values.password}
+                        secureTextEntry={hidePassword}
+                        isPassword={true}
+                        hidePassword={hidePassword}
+                        setHidePassword={setHidePassword}
+                    />
+                    <MsgBox>...</MsgBox>
+                    <StyledButton>
+                        <ButtonText>Register</ButtonText>
+                    </StyledButton>
+                    <Line/>
+                    <StyledButton google={true} onPress={()=>{}}>
+                        <Fontisto name='google' color={primary} size={25} />
+                        <ButtonText google={true}>Signin with google</ButtonText>
+                    </StyledButton>
+                    </StyledFormArea>}
+            </Formik>
+        </InnerContainer>
+    </StyledContainer>
   )
 }
 
